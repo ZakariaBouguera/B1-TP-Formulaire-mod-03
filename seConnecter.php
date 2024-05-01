a1848ac01d7e3d4d0189ca8a57a3ef584cc08b17
ZakariaBouguera <146200319+ZakariaBouguera@users.noreply.github.com>
2024-05-01 14:23:45 +0200

    Add files via upload

---
+++ seConnecter.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Sanaya</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+
+    <body>
+		
+		
+		<?php
+
+			$connexionOk = FALSE ;
+			$nomConnexion = ($_POST['nomConnexion']);
+			$motDePasse = ($_POST['motDePasse']);
+			
+			if( $nomConnexion == "Sanaya" && $motDePasse == "bio" ){
+				$connexionOk = TRUE ;
+			}
+		
+		?>
+		
+		<h5>
+			<?php
+				if( $connexionOk){
+					echo "Administrateur connecté." ;
+				}
+				else {
+					echo "Connexion refusée." ;
+				}
+			
+			?>
+		</h5>
+		<a href="authentification.html">
+		
+		<?php
+		
+			if( $connexionOk ){
+				echo "Déconnexion" ;
+			}
+			else {
+				echo '<a href ="authentification.html"> Retourner au formulaire  </a>';
+			}
+		
+		?>
+		</a>
+		
+	</body>
+
+</html>	
